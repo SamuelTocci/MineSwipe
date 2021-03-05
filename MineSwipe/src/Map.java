f8cc21b80ba4dae2df15be5fe1308ff61125643c
glenn <glennnyss@gmail.com>
2021-03-05 12:48:00 +0100

    map aanpassing

--- MineSwipe/src/Map.java
+++ MineSwipe/src/Map.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Map {
+    private ArrayList<ArrayList<String>> map;
+
+    public Map(int sizeX, int sizeY, int nOfMines){
+
+        for (int i = 0, i<sizeX, i++){
+            for (int j = 0, j<sizeY, j++){
+
+            }
+        }
+    }
+
+    private void addMines(int nOfMines){
+
+    }
+
+    private void calculateNumbers)(){
+
+    }
 }
+
+
