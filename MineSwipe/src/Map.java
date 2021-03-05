bd8b702fb6c83c5d3cfb6ab74d637a26508cd1f4
glenn <glennnyss@gmail.com>
2021-03-05 13:29:40 +0100

    werkende 2d arraymaker met kiesbare grote

--- MineSwipe/src/Map.java
+++ MineSwipe/src/Map.java
@@ -3,22 +3,21 @@ import java.util.ArrayList;
 public class Map {
     private ArrayList<ArrayList<String>> map;
 
-    public Map(int sizeX, int sizeY, int nOfMines){
+    public Map(int sizeX, int sizeY, int nOfMines) {
 
-        for (int i = 0, i<sizeX, i++){
-            for (int j = 0, j<sizeY, j++){
+        map = new ArrayList<ArrayList<String>>();
 
+        for (int i = 0; i < sizeX; i++) {
+            map.add(new ArrayList<String>());
+        }
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+                map.get(i).add("0");
             }
         }
-    }
-
-    private void addMines(int nOfMines){
-
-    }
-
-    private void calculateNumbers)(){
-
+        System.out.print(map);
     }
 }
+
 
 
