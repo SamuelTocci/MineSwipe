1b9daaeefd26e47dbc3b4fd11ed63d5a81c94338
Samuel Tocci <samuel.tocci@gmail.com>
2021-03-14 20:38:37 +0100

    resource folder aangemaakt voor GUI icoontjes en shit

--- MineSwipe/src/GUI.java
+++ MineSwipe/src/GUI.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 import java.awt.geom.Point2D;
-import java.util.*;
 import java.awt.*;
 
 public class GUI extends JFrame {
+    private GameState m_curState = GameState.TITLE_STATE;
 
     public GUI() {
         this.setTitle("MineSwipe");
@@ -19,20 +19,38 @@ public class GUI extends JFrame {
 
     public class Board extends JPanel {
         public void paintComponent(Graphics g) {
-            Color color1 = new Color(253, 41, 123);
-            Color color2 = new Color(255, 88, 100);
-            Color color3 = new Color(255, 101, 91);
+            switch(m_curState) {
+                case TITLE_STATE:
+                    Color color1 = new Color(253, 41, 123);
+                    Color color2 = new Color(255, 88, 100);
+                    Color color3 = new Color(255, 101, 91);
 
-            super.paintComponent(g);
-            Graphics2D g2d = (Graphics2D) g;
+                    super.paintComponent(g);
+                    Graphics2D g2d = (Graphics2D) g;
 
-            Point2D start = new Point2D.Float(0, 540);
-            Point2D end = new Point2D.Float(1920, 540);
-            float[] dist = {0.0f, 0.5f, 1.0f};
-            Color[] colors = {color1, color2, color3};
-            LinearGradientPaint gradient = new LinearGradientPaint(start, end, dist, colors);
-            g2d.setPaint(gradient);
-            g2d.fillRect(0,0,1920,1080);
+                    Point2D start = new Point2D.Float(0, 540);
+                    Point2D end = new Point2D.Float(1920, 540);
+                    float[] dist = {0.0f, 0.5f, 1.0f};
+                    Color[] colors = {color1, color2, color3};
+                    LinearGradientPaint gradient = new LinearGradientPaint(start, end, dist, colors);
+                    g2d.setPaint(gradient);
+                    g2d.fillRect(0,0,1920,1080);
+
+                    Toolkit t=Toolkit.getDefaultToolkit();
+                    Image i=t.getImage("LOGO.png");
+                    g.drawImage(i, 0,0,this);
+
+                case MAINGAME_STATE:
+                    UpdateMainGame();
+                    break;
+
+                case DEATH_STATE:
+                    UpdateDeath();
+                    if(UserPressesRetry()) {
+                        m_curState = GameState.MAINGAME_STATE;
+                    }
+                    break;
+            }
         }
     }
     public enum GameState {
@@ -40,26 +58,40 @@ public class GUI extends JFrame {
         MAINGAME_STATE,
         DEATH_STATE,
     }
-    private GameState m_curState = GameState.TITLE_STATE
-    void GameStateUpdate() {
+    public void GameStateUpdate() {
         // handle update
         switch(m_curState) {
             case TITLE_STATE:
                 UpdateTitleScreen();
                 if(UserPressesEnter()) {
-                    m_curState = MAINGAME_STATE;
+                    m_curState = GameState.MAINGAME_STATE;
                 }
                 break;
             case MAINGAME_STATE:
                 UpdateMainGame();
                 break;
-            case PAUSE_STATE:
+            case DEATH_STATE:
                 UpdateDeath();
                 if(UserPressesRetry()) {
-                    m_curState = MAINGAME_STATE;
+                    m_curState = GameState.MAINGAME_STATE;
                 }
                 break;
         }
+    }
+
+    private boolean UserPressesRetry() {
+        return false;
+    }
+
+    private boolean UserPressesEnter() {
+        return false;
+    }
+
+    private void UpdateTitleScreen() {
+    }
+    private void UpdateMainGame() {
+    }
+    private void UpdateDeath() {
     }
 
 }
